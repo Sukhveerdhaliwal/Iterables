a05258c2ed3a87127e8dc648c7556acff84ae1b1
Sukhveer Dhaliwal <kaursukhveer21@gmail.com>
2026-04-13 20:41:21 -0400

    Assignment 6

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,24 @@
-
+class fibonacci:
+    def __init__(self, n):
+        if not isinstance(n, int):
+            raise ValueError("Input must be an integer")
+        self.n = n
+    def __iter__(self):
+        self.count = 0
+        self.a, self.b = 0, 1
+        return self
+    def __next__(self):
+        if self.n < 0:
+            raise StopIteration
+        if self.count > self.n:
+            raise StopIteration
+        if self.count == 0:
+            self.count += 1
+            return 0
+        elif self.count == 1:
+            self.count += 1
+            return 1
+        else:
+            val = self.a + self.b
+            self.a, self.b = self.b, val
+            return val
--- test_Fibonacci.py
+++ test_Fibonacci.py
@@ -1,1 +1,20 @@
+import pytest
+from Fibonacci import fibonacci
+
+def test_invalid_input():
+    with pytest.raises(ValueError):
+        fibonacci("Input is not an integer")
+def test_negative_input():
+    assert list(fibonacci(-1)) == []
+def test_zero_input():
+    assert list(fibonacci(0)) == [0]
+def test_one_input():
+    assert list(fibonacci(1)) == [0, 1]
+def test_two_input():
+    assert list(fibonacci(2)) == [0, 1, 1]
+def test_four_input():
+    assert list(fibonacci(4)) == [0, 1, 1, 2, 3]
+def test_ten_input():
+    assert list(fibonacci(10)) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
 
